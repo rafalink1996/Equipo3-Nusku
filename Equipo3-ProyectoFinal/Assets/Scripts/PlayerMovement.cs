1b62c6a97c3bd73cace682d7fa4093566741035f
Rafael Ospina <rafaelospiname@gmail.com>
2018-03-03 20:05:45 -0500

    Sora se mueve. Pero no está animado.

---
+++ Equipo3-ProyectoFinal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float speed;
+	public bool diagonal = false; 
+	public bool isRunning = false;
+	public float walkSpeed;
+	public float runSpeed;
+	public string up;
+	public string down;
+	public string right;
+	public string left;
+	public string run;
+
+
+	void Start () {
+		
+	}
+		
+	void FixedUpdate () {
+		if (Input.GetKey (up)) {
+			transform.Translate (0, 0, speed * Time.deltaTime);
+		}
+		if (Input.GetKey (down)) {
+			transform.Translate (0, 0, -speed * Time.deltaTime);
+		}
+		if (Input.GetKey (right)) {
+			transform.Translate (speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (left)) {
+			transform.Translate (-speed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (run)) {
+			isRunning = true;
+		} else {
+			isRunning = false;
+		}
+		if (isRunning == true) {
+			speed = runSpeed;
+		} else {
+			speed = walkSpeed;
+		}
+		if (Input.GetKey (up) && Input.GetKey (right) || Input.GetKey (up) && Input.GetKey (left) || Input.GetKey (down) && Input.GetKey (right) || Input.GetKey (down) && Input.GetKey (left)) {
+			diagonal = true;
+		} else {
+			diagonal = false;
+		}
+	}
+}
