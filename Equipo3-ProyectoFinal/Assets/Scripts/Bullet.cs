6d735b6a6dba30a508d1cd74248c406ea5ddc50b
Rafael Ospina <rafaelospiname@gmail.com>
2018-05-23 12:20:37 -0500

    Vuelvo a agregar la última versión de Pablo

---
+++ Equipo3-ProyectoFinal/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+
+    float speed = 30.0f;
+
+    string attack;
+    //bool attackIsPressed = true;
+
+	// Use this for initialization
+	void Start () {
+        attack = GameObject.Find("Sel").GetComponent<PlayerMovement>().attack;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       /* if (Input.GetKey(attack)){
+            this.transform.SetParent(GameObject.Find("Glove").transform);
+        }*/
+
+        /*if (Input.GetKeyUp(attack))
+        {
+            //attackIsPressed = false;
+            this.transform.SetParent(null);
+        }*/
+
+       /* if (attackIsPressed == false)
+        {
+            this.transform.Translate(speed * Time.deltaTime, 0, 0);
+        }*/
+        if (transform.parent == null){
+            this.transform.Translate(speed * Time.deltaTime, 0, 0);
+            Destroy(this.gameObject, 1);
+        }
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag != "Player" && this.transform.parent == null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Equipo3-ProyectoFinal/Assets/Scripts/SliderTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderTest : MonoBehaviour {
+
+    public Slider sliderHealth;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        sliderHealth.value = GetComponent<PlayerMovement>().health;
+
+    }
+
+}
