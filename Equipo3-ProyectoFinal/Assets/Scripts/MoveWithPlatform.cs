a1363e1f94fc7663ed5ed5826dde93b561e4f392
Unknown <interdimensionaldolphin@gmail.com>
2018-05-25 13:17:12 -0500

    Push con el proyecto de MIla otra vez por hp

---
+++ Equipo3-ProyectoFinal/Assets/Scripts/MoveWithPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // this.transform.rotation = Quaternion.Euler(0, 0, 0, Space.World);
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Player"){
+            collision.collider.transform.SetParent(transform);
+        }
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+
+    }
+}
