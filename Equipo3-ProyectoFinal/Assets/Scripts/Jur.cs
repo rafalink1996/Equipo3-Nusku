513a79a487541d03678677fd9f203dc663870c44
Rafael Ospina <rafaelospiname@gmail.com>
2018-05-25 20:46:38 -0500

    Mega push. Primera versión de ataques de Jur

---
+++ Equipo3-ProyectoFinal/Assets/Scripts/Jur.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jur : MonoBehaviour {
+
+    int health = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print("health = " + health); 
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet"){
+            health = health - 10;
+        }
+    }
+    public void Attack(){
+        GameObject.Find("Mouth").GetComponent<JurMouth>().JurAttack();
+    }
+}
