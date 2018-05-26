deb5ec40e6c161e65364a6c99fc790145e47ceab
Juan Felipe Medrano <juanfe-medrano@hotmail.com>
2018-05-26 01:37:34 -0500

    Push de transicion de nivel 1 a nivel 2

---
+++ Equipo3-ProyectoFinal/Assets/Scripts/EnterTriggerNewLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnterTriggerNewLevel : MonoBehaviour {
+
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+        other.GetComponent<LevelChanger2>().FadeToLevel(2);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+       
+	}
+}
