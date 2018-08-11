aae0741b5c98f673412bab8762994b64e724e083
Rafael Ospina <rafaelospiname@gmail.com>
2018-08-11 12:15:37 -0500

    Ya los ataques de Jur pueden ocurrir aleatoriamente

---
+++ Nusku/Assets/Scripts/Jur2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jur2D : MonoBehaviour {
+
+    float timeToAttack;
+    Animator anim;
+    bool isAttacking = false;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        timeToAttack = Random.Range(1.0f, 4.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeToAttack -= Time.deltaTime;
+        if (timeToAttack <= 0 && isAttacking == false){
+            anim.SetFloat("attackType", Random.Range(0, 3));
+            anim.SetTrigger("Attack");
+            isAttacking = true;
+            anim.SetBool("isAttacking", true);
+        }
+        print("timeToAttack = " + timeToAttack);
+	}
+    void resetTimer (){
+        timeToAttack = Random.Range(1.0f, 4.0f);
+        isAttacking = false;
+        anim.SetBool("isAttacking", false);
+    }
+}
