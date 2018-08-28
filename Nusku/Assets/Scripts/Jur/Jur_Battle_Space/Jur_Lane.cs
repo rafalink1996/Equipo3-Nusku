b291cd8dd940d8730693de5b082fe2d9cd486577
Rafael Ospina <rafaelospiname@gmail.com>
2018-08-27 20:00:49 -0500

    no se

---
+++ Nusku/Assets/Scripts/Jur/Jur_Battle_Space/Jur_Lane.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jur_Lane : MonoBehaviour {
+
+    public float direction;
+    //public Animator jur;
+    Jur2D jur;
+	// Use this for initialization
+	void Start () {
+        jur = FindObjectOfType<Jur2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            jur.direction = direction;
+        }
+    }
+}
