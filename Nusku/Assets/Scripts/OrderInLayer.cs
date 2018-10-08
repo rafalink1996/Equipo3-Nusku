fbb0f4d846776e4b343520930df8c05180335903
Rafael Ospina <rafaelospiname@gmail.com>
2018-10-08 15:22:55 -0500

    Pasar detrás de las cosas

---
+++ Nusku/Assets/Scripts/OrderInLayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderInLayer : MonoBehaviour {
+
+    Transform sel;
+    public int frontLayer;
+    public int behindLayer;
+	// Use this for initialization
+	void Start () {
+        sel = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (sel.position.y < transform.position.y)
+        {
+            GetComponent<SpriteRenderer>().sortingOrder = behindLayer;
+        }else{
+            GetComponent<SpriteRenderer>().sortingOrder = frontLayer;
+        }
+	}
+}
