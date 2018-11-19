f0004a892de9c4ac13140d1e860799c7f265ea97
Rafael Ospina <rafaelospiname@gmail.com>
2018-11-19 14:11:15 -0500

    transiciones de escenas

---
+++ Nusku/Assets/Scripts/GameStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStats : MonoBehaviour {
+
+    public static GameStats stats;
+    public Vector2 destination;
+    public int selDirectionX;
+    public int selDirectionY;
+    public int health;
+    SelHealth selHealth;
+	// Use this for initialization
+	void Awake () {
+        if (stats == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            stats = this;
+        }
+        else if (stats != this){
+            Destroy(gameObject);
+        }
+        selHealth = FindObjectOfType<SelHealth>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
--- Nusku/Assets/Scripts/SceneChange.cs
+++ Nusku/Assets/Scripts/SceneChange.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour {
 
+    public string sceneToLoad;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,12 @@ public class SceneChange : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
 }
