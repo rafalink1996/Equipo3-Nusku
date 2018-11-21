6eacb72fed4ee34ab22fbc5a04ac1a133462f5dc
Rafael Ospina <rafaelospiname@gmail.com>
2018-11-20 19:08:25 -0500

    Nieve

---
+++ Nusku/Assets/Scripts/Snow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
