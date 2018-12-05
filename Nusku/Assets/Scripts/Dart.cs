b97f382091b5f35e4f0a754ba680a9914e74d10a
Rafael Ospina <rafaelospiname@gmail.com>
2018-12-05 17:43:05 -0500

    Tiro al blanco

---
+++ Nusku/Assets/Scripts/Dart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour {
+
+    ShootingRange shoot;
+    bool isShot;
+	// Use this for initialization
+	void Start () {
+        shoot = FindObjectOfType<ShootingRange>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (shoot.shooting && !isShot)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, shoot.target, 9 * Time.deltaTime);
+        }
+        if (transform.position == new Vector3(shoot.target.x, shoot.target.y, 0))
+        {
+            isShot = true;
+        }
+	}
+}
--- Nusku/Assets/Scripts/ShootingRange.cs
+++ Nusku/Assets/Scripts/ShootingRange.cs
@@ -8,17 +8,20 @@ public class ShootingRange : MonoBehaviour
 
 
     public Slider slider;
+    public float score;
     bool right = true;
     public float speed;
     float aim;
-    int shots = 3;
+    int shots = 5;
     bool shot;
-    bool shooting;
+    public bool shooting;
+    public Vector2 target;
     // Use this for initialization
     void Start()
     {
         aim = speed;
         slider.value = 0;
+
     }
 
     // Update is called once per frame
@@ -49,36 +52,42 @@ public class ShootingRange : MonoBehaviour
         {
             if (slider.value >= 49 && slider.value <= 51)
             {
-                print("1000");
+                score = score + 1000;
+                target = new Vector2(4.817f, 0.637f);
             }
             if (slider.value >= 40 && slider.value <= 48 || slider.value >= 52 && slider.value <= 60)
             {
-                print("800");
+                score = score + 800;
+                target = new Vector2(Random.Range(4.62f, 5.13f), Random.Range(0.31f, 1.03f));
             }
             if (slider.value >= 30 && slider.value <= 39 || slider.value >= 61 && slider.value <= 70)
             {
-                print("600");
+                score = score + 600;
+                target = new Vector2(Random.Range(4.62f, 5.13f), Random.Range(0.31f, 1.03f));
             }
             if (slider.value >= 20 && slider.value <= 29 || slider.value >= 71 && slider.value <= 80)
             {
-                print("400");
+                score = score + 400;
+                target = new Vector2(Random.Range(4.62f, 5.13f), Random.Range(0.31f, 1.03f));
             }
             if (slider.value >= 10 && slider.value <= 19 || slider.value >= 81 && slider.value <= 90)
             {
-                print("200");
+                score = score + 200;
+                target = new Vector2(Random.Range(4.62f, 5.13f), Random.Range(0.31f, 1.03f));
             }
             if (slider.value >= 5 && slider.value <= 9 || slider.value >= 91 && slider.value <= 96)
             {
-                print("100");
+                score = score + 100;
+                target = new Vector2(Random.Range(4.62f, 5.13f), Random.Range(0.31f, 1.03f));
             }
             if (slider.value <= 4 || slider.value >= 97)
             {
-                print("0");
+                target = new Vector2(Random.Range(4.325f, 4.425f), Random.Range(1.065f, 1.165f));
             }
             shot = false;
         }
     }
-    void Shoot()
+    public void Shoot()
     {
 
         aim = 0;
@@ -93,8 +102,10 @@ public class ShootingRange : MonoBehaviour
     }
     void Reload()
     {
-        slider.value = 0;
+        slider.value = Random.Range(0, 100f);
         aim = speed;
         shooting = false;
+        GameObject dart = GameObject.Instantiate(Resources.Load("Prefabs/Dart") as GameObject);
+        dart.transform.position = new Vector2(2.512f, -1.123f);
     }
 }
